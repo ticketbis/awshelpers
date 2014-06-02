9de7d084eb53c7792d931fda3cfde520276ad512
eloygbm <eloygbm@gmail.com>
2014-06-02 23:06:34 +0200

    Mejoras en el setup de instalacion

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup, find_packages
+
+requires_file = open('requirements.txt','r')
+# print requires_file.readlines()
 
 setup(name='AwsHelpers',
     version='0.1',
     description='AWS Helpers - to accelerate hard jobs with AWS',
     author='Eloy Garcia-Borreguero',
     author_email='eloygbm@gmail.com',
-    packages=find_packages(),
     platforms='any',
     scripts=['bin/awshelpers'],
-    py_modules=['awshelpers/lib/awsroute53helper','awshelpers/lib/__init__','awshelpers/main','awshelpers/local_settings','awshelpers/__init__']
+    install_requires=requires_file.readlines(),
+    packages=find_packages(),
+    zip_safe=False
     )
+
+# ,
+    # py_modules=['awshelpers/lib/awsroute53helper','awshelpers/lib/__init__','awshelpers/main','awshelpers/local_settings','awshelpers/__init__']
